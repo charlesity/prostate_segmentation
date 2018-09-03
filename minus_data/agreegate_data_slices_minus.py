5769178d75c6f0f7c24d191ec3fe02cc7fc1681e
Charley <charles.i.saidu@gmail.com>
2018-09-03 14:28:11 +0300

    n

--- minus_data/agreegate_data_slices_minus.py
+++ minus_data/agreegate_data_slices_minus.py
@@ -11,9 +11,9 @@ from tqdm import tqdm
 from sklearn.preprocessing import Normalizer
 
 
-XY_Data = np.load("./Processed_data/XY_Dataset_28_28.npy")
+XY_Data = np.load("./Processed_data/XY_Dataset_28_28_with_minus_1.npy")
 slice_list = np.unique(XY_Data[:, XY_Data.shape[1]-1])
-
+# print (slice_list)
 save_location = './Processed_data/slices/'
 # print (slice_list)
 #
@@ -21,4 +21,7 @@ for s in slice_list:
     location = XY_Data[:, XY_Data.shape[1]-1] == s
     data = XY_Data[location]
     data[:, :(28**2)] = Normalizer().fit_transform(data[:, :(28**2)] + 1)
+
+    # print (data[0, :50], data[:5].shape, data[0, :(28**2)].min())
+    # break
     np.save(save_location+'pslice_'+str(int(s)), data)
