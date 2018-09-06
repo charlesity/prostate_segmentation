e341b24cbf4d902255f07380225f1be8165661ff
Charley <charles.i.saidu@gmail.com>
2018-09-06 16:27:24 +0300

    run scripts

---
+++ cropped_data_experiment/run_random.sh
@@ -0,0 +1,13 @@
+#~/bin/bash
+
+echo "Welcome"
+sleep 1
+echo "Starting script"
+
+source ~/charles/environments/keras0/bin/activate
+
+echo "Running script without oversampling"
+python ~/charles/prostate_segmentation/cropped_data_experiment/active_deep_seg_random.py -ds_type 0
+
+echo "Running script with oversampling"
+python ~/charles/prostate_segmentation/cropped_data_experiment/active_deep_seg_random.py -ds_type 0 -ovs
