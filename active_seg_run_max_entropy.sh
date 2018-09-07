2f663240dc5708248be8a24e21ecaeb1273f39af
Charley <charles.i.saidu@gmail.com>
2018-09-07 13:31:33 +0300

    bash scripts

---
+++ active_seg_run_max_entropy.sh
@@ -0,0 +1,11 @@
+#~/bin/bash
+
+echo "Welcome"
+sleep 1
+echo "Starting script"
+
+
+source ~/charles/environments/keras0/bin/activate
+
+echo "Running script on 60 by 60 zero based data"
+python ~/charles/prostate_segmentation/active_deep_seg_max_entropy.py
