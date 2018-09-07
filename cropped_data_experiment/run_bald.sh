6c58c2f5efbc5602a527b21c96582b5c24099454
Charles <charles.i.saidu@gmail.com>
2018-09-07 10:13:53 +0300

    u

--- cropped_data_experiment/run_bald.sh
+++ cropped_data_experiment/run_bald.sh
@@ -7,7 +7,7 @@ echo "Starting script"
 source ~/charles/environments/keras0/bin/activate
 
 echo "Running script without oversampling"
-python ~/charles/prostate_segmentation/cropped_data_experiment/active_deep_seg_bald.py -ds_type 0
+# python ~/charles/prostate_segmentation/cropped_data_experiment/active_deep_seg_bald.py -ds_type 0
 
 echo "Running script with oversampling"
 python ~/charles/prostate_segmentation/cropped_data_experiment/active_deep_seg_bald.py -ds_type 0 -ovs
